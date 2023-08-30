dee658f9fc3ab659c769cbd25dc338a7a9f400ba
Renzo <jrenzoanicama5@gmail.com>
2023-08-30 15:09:56 -0500

    retocando f2

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
 
 ### Descripción general
 
-* Figma
-* Imágenes
+- Figma
+- Imágenes
 
 **Figma**
 
-* [Enlace al proyecto en Figma](https://www.figma.com/file/LDMgqWesKpQkIwhOfEBuTS/WEB%2C-Sprint-5%3A-Around-The-U.S.-%7C-desktop-%2B-mobile?node-id=0%3A1)
+- [Enlace al proyecto en Figma](https://www.figma.com/file/LDMgqWesKpQkIwhOfEBuTS/WEB%2C-Sprint-5%3A-Around-The-U.S.-%7C-desktop-%2B-mobile?node-id=0%3A1)
 
 **Imágenes**
 
-A la hora de trabajar, debes optar por exportar las imágenes directamente desde Figma; te recomendamos hacerlo para practicar más. No olvides optimizarlas [aquí](https://tinypng.com/), para que tu proyecto cargue más rápido. 
+A la hora de trabajar, debes optar por exportar las imágenes directamente desde Figma; te recomendamos hacerlo para practicar más. No olvides optimizarlas [aquí](https://tinypng.com/), para que tu proyecto cargue más rápido.
 
-¡Esperamos que te diviertas con JavaScript!
+En este proyecto se creó una página interactiva en la que los usuarios pueden añadir, eliminar o dar "me gusta" a las fotos.
+
+Toda la maquetación y estructura del proyecto se realizo en el editor de código [Visual estudio Code](https://code.visualstudio.com/).
+Se utilizaron los siguientes lenguajes:
+*HTML
+*CSS
+\*JAVASCRIPT
+[Enlace a Git](https://github.com/Renzor2d2)
--- script.js
+++ script.js
@@ -2,44 +2,36 @@ let profileButton = document.querySelector(".profile-info__button");
 let popup = document.querySelector(".popup");
 let popupOpen = popup.querySelector(".popup_oppened");
 let popupContainer = popup.querySelector(".popup-container");
-let submitButton = popupContainer.querySelector(".popup-container__submit");
+let submitButton = popupContainer.querySelector(".input__submit");
 let closeIcon = popup.querySelector(".popup__close-icon");
 let page = document.querySelector(".page");
 
-// let header = document.querySelector(".header");
-// let main = document.querySelector(".main");
-// let footer = document.querySelector(".footer");
-
 function open() {
-  popup.classList.remove("popup_oppened");
-  page.setAttribute("style", "opacity:0.5");
-  // header.setAttribute("style", "opacity:0.5");
-  // main.setAttribute("style", "opacity:0.5");
-  // footer.setAttribute("style", "opacity:0.5");
+	popup.classList.remove("popup_oppened");
+	// 	page.setAttribute("style", "opacity:0.5");
+	page.classList.add("page_opacity");
 }
 
 profileButton.addEventListener("click", open);
 
 function close() {
-  popup.classList.add("popup_oppened");
-  page.removeAttribute("style", "opacity:0.5");
-  // header.removeAttribute("style", "opacity:0.5");
-  // main.removeAttribute("style", "opacity:0.5");
-  // footer.removeAttribute("style", "opacity:0.5");
+	popup.classList.add("popup_oppened");
+	// page.removeAttribute("style", "opacity:0.5");
+	page.classList.remove("page_opacity");
 }
 
 closeIcon.addEventListener("click", close);
 
 function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileTitle = document.querySelector(".profile-info__title");
-  profileSubtitle = document.querySelector(".profile-info__subtitle");
+	evt.preventDefault();
+	profileTitle = document.querySelector(".profile-info__title");
+	profileSubtitle = document.querySelector(".profile-info__subtitle");
 
-  let nameInput = popupContainer.querySelector(".input__name");
-  let jobInput = popupContainer.querySelector(".input__job");
+	let nameInput = popupContainer.querySelector(".input__name");
+	let jobInput = popupContainer.querySelector(".input__job");
 
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
-  submitButton.addEventListener("click", close);
+	profileTitle.textContent = nameInput.value;
+	profileSubtitle.textContent = jobInput.value;
 }
 popup.addEventListener("submit", handleProfileFormSubmit);
+submitButton.addEventListener("click", close);
